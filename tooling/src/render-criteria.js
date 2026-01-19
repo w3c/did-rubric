2fe0e03127c7d6c02ce9b790027ce2c695363e64
Will Abramson <wip.abramson@gmail.com>
2026-01-19 15:23:39 +0000

    generate possible responses using labels not autogenerated

--- tooling/src/render-criteria.js
+++ tooling/src/render-criteria.js
@@ -35,76 +35,72 @@ function renderCriteria(criteria) {
     let criteriaIdFragment = criteria.id.split('#')[1];
     // TODO: Add ID to this html element so you can link to it.
     return `
-    <section class="criteria">
-       <h4 id="${criteriaIdFragment}">${criteria.name}</h4>
-       <a href="#${criteriaIdFragment}">${criteria.id}</a>
-       <span>v${criteria.version}</span>
+        <section class="criteria">
+           <h4 id="${criteriaIdFragment}">${criteria.name}</h4>
+           <a href="#${criteriaIdFragment}">${criteria.id}</a>
+           <span>v${criteria.version}</span>
 
-        ${(criteria.source && criteria.source !== ".") ? `
+            ${(criteria.source && criteria.source !== ".") ? `
             <div class="source">
                 <p>Source: <a href="${criteria.source}">${criteria.source}</a></p>
             </div>
-        ` : ''}
+            ` : ''}
 
 
-        <section>
-            <h5>Question</h4>
+            <section>
+            <h5>Question</h5>
             <p>${criteria.question.question}</p>
             ${criteria.question.instruction ?
                 `<p><span>Instruction:</span>${criteria.question.instruction}</p>`
             : ``}
-        </section>
+            </section>
 
-        ${criteria.response && criteria.response.possibleResponses ?
+            ${criteria.response && criteria.response.possibleResponses ?
             `<section>
                 <h5>Responses</h5>
-                <ol type="A">
-                    ${criteria.response.possibleResponses.map(response => `
-                        <li>${response.meaning}</li>
-                    `).join('')}
-                </ol>
-
-
+                <ul class="responses" style="list-style-type: none; padding: 0;">
+                ${criteria.response.possibleResponses.map(response => `
+                    <li><span class="response-label">${response.label}:</span> ${response.meaning || ''}</li>
+                `).join('')}
+                </ul>
             </section>` : ''}
 
-        ${criteria.relevance &&
+            ${criteria.relevance &&
             `<section>
                 <h5>Relevance</h5>
                 <p>${criteria.relevance}</p>
             </section>`}
 
-        ${criteria.exampleAssessments && criteria.exampleAssessments.length > 0 ? `<section>
+            ${criteria.exampleAssessments && criteria.exampleAssessments.length > 0 ? `<section>
             <h5>Examples</h5>
             <table>
             <thead>
                 <tr>
-                    <!-- <th>ID</th> -->
-                    ${criteria.assessmentTemplate && criteria.assessmentTemplate.columns && criteria.assessmentTemplate.columns.map(column => `<th>${column.heading}</th>`).join('')}
+                ${criteria.assessmentTemplate && criteria.assessmentTemplate.columns ? criteria.assessmentTemplate.columns.map(column => `<th>${column.heading}</th>`).join('') : ''}
                 </tr>
             </thead>
             <tbody>
-                ${criteria.exampleAssessments && criteria.exampleAssessments.map(assessment => `
-                    <tr>
-                        <!-- <td>${assessment.id}</td> -->
-                        ${criteria.assessmentTemplate.columns && criteria.assessmentTemplate.columns.map(column => {
-                            let contents = assessment[column.propertyRef];
-                            if (column.propertyRef === 'note') {
-                                let evalCitation = `[<a href="${assessment.evaluationCitation}">${assessment.evaluationCitation.slice(1)}</a>]`
+                ${criteria.exampleAssessments.map(assessment => `
+                <tr>
+                    ${criteria.assessmentTemplate && criteria.assessmentTemplate.columns ? criteria.assessmentTemplate.columns.map(column => {
+                    let contents = assessment[column.propertyRef] || '';
+                    if (column.propertyRef === 'note' && assessment.evaluationCitation) {
+                        let evalCitation = `[<a href="${assessment.evaluationCitation}">${assessment.evaluationCitation.slice(1)}</a>]`
 
-                                contents += ` ${evalCitation}`;
-                            }
+                        contents += ` ${evalCitation}`;
+                    }
 
-                            return `<td>${contents}</td>`;
-                        }).join('')}
-                    </tr>
+                    return `<td>${contents}</td>`;
+                    }).join('') : ''}
+                </tr>
                 `).join('')}
             </tbody>
             </table>
 
-        </section>` : ''}
+            </section>` : ''}
 
-    </section>
-    `
+        </section>
+        `
 
 }
 
