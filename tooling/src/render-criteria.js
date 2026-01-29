cbfa4ee657c199d118ce71f50294030d1cedbaab
Joe Andrieu <joe@andrieu.net>
2026-01-29 10:45:35 -0800

    first pass at new rubric tables; broken still

--- tooling/src/render-criteria.js
+++ tooling/src/render-criteria.js
@@ -1,8 +1,11 @@
 
 
 async function renderCategories() {
-  const {document} = window;
+  const { document } = window;
   const criteriaSection = document.getElementById('criteria');
+  // this section must already exist in the document, 
+  // inside  a <section> element.
+
 
   if (!criteriaSection) {
     console.warn('No criteria section found');
@@ -16,92 +19,199 @@ async function renderCategories() {
   const rubric = rubricJson;
 
   let categoriesHtml = rubric.categories.map(category => {
-    return `<section class="category-section">
+    return `<section class="category">
                 <h3>${category.name}</h3>
-                <p>${category.description}</p>
+                <p class="description">${category.description}</p>
                 ${(category.criteria || []).map(criteria => renderCriteria(criteria)).join('')}
             </section>`
   }).join('');
 
-  criteriaSection.innerHTML = `<h2>The criteria</h2>` + categoriesHtml;
+  criteriaSection.innerHTML = `<h2>The Criteria</h2>` + categoriesHtml;
 
 }
-
-
-
 
 function renderCriteria(criteria) {
+  let criteriaIdFragment = criteria.id.split('#')[1];
+  console.log(`rendering ${criteriaIdFragment}`);
+  return `
+      <section id="${criteriaIdFragment}" class="criterion">
+      ${nameRow(criteria)}
+      ${idRow(criteria)}
+      ${versionRow(criteria)}
+      ${questionRow(criteria)}
+      ${instructionRow(criteria)}
+      ${responsesRow(criteria)}
+      ${relevanceRow(criteria)}
+      ${assessmentsRow(criteria)}
+      ${sourceRow(criteria)}
+      </section>
 
-    let criteriaIdFragment = criteria.id.split('#')[1];
-    // TODO: Add ID to this html element so you can link to it.
-    return `
-        <section class="criteria">
-           <h4 id="${criteriaIdFragment}">${criteria.name}</h4>
-           <a href="#${criteriaIdFragment}">${criteria.id}</a>
-           <span>v${criteria.version}</span>
+  `;
+}
 
-            ${(criteria.source && criteria.source !== ".") ? `
-            <div class="source">
-                <p>Source: <a href="${criteria.source}">${criteria.source}</a></p>
-            </div>
-            ` : ''}
+function nameHeading(criteria) {
+  return `<h4 class="hide-heading">${criteria.name}</h4>`;
+}
+function nameRow(criteria) {
 
+  return `
+<h4 class="name-row" >${criteria.name}</h4> 
+    `;
+}
 
-            <section>
-            <h5>Question</h5>
-            <p>${criteria.question.question}</p>
-            ${criteria.question.instruction ?
-                `<p><span>Instruction:</span>${criteria.question.instruction}</p>`
-            : ``}
-            </section>
+function idRow(criteria) {
+  let criteriaIdFragment = criteria.id.split('#')[1];
+  return `
+    <div class="id-row"> 
+        <span class="label">ID:</span>
+        <a class="id" href="#${criteriaIdFragment}">${criteria.id}</a>
+    </div>
+    `;
+}
 
-            ${criteria.response && criteria.response.possibleResponses ?
-            `<section>
-                <h5>Responses</h5>
-                <ul class="responses" style="list-style-type: none; padding: 0;">
-                ${criteria.response.possibleResponses.map(response => `
-                    <li><span class="response-label">${response.label}:</span> ${response.meaning || ''}</li>
-                `).join('')}
-                </ul>
-            </section>` : ''}
-
-            ${criteria.relevance &&
-            `<section>
-                <h5>Relevance</h5>
-                <p>${criteria.relevance}</p>
-            </section>`}
-
-            ${criteria.exampleAssessments && criteria.exampleAssessments.length > 0 ? `<section>
-            <h5>Examples</h5>
-            <table>
-            <thead>
-                <tr>
-                ${criteria.assessmentTemplate && criteria.assessmentTemplate.columns ? criteria.assessmentTemplate.columns.map(column => `<th>${column.heading}</th>`).join('') : ''}
-                </tr>
-            </thead>
-            <tbody>
-                ${criteria.exampleAssessments.map(assessment => `
-                <tr>
-                    ${criteria.assessmentTemplate && criteria.assessmentTemplate.columns ? criteria.assessmentTemplate.columns.map(column => {
-                    let contents = assessment[column.propertyRef] || '';
-                    if (column.propertyRef === 'note' && assessment.evaluationCitation) {
-                        let evalCitation = `[<a href="${assessment.evaluationCitation}">${assessment.evaluationCitation.slice(1)}</a>]`
-
-                        contents += ` ${evalCitation}`;
-                    }
-
-                    return `<td>${contents}</td>`;
-                    }).join('') : ''}
-                </tr>
-                `).join('')}
-            </tbody>
-            </table>
-
-            </section>` : ''}
-
-        </section>
-        `
+function versionRow(criteria) {
+  return `
+    <div class="version-row">
+        <span class="label">Version:</span>
+        <span class="version">v${criteria.version}</span>
+    </div>`;
 
 }
+
+function sourceRow(criteria) {
+  if ((typeof criteria.source === "undefined") ||
+    criteria.source === ".") {
+    return ``;
+  } else
+    return `
+    <div class="source-row">
+        <span class="label">Source:</span>
+        <a href="${criteria.source}">${criteria.source}</a>
+    </div>
+    `;
+}
+
+function questionRow(criteria) {
+  return `
+  <div class="question-row">
+    ${criteria.question.question}</a>
+  </div>
+  `;
+}
+
+function instructionRow(criteria) {
+  if ((typeof criteria.question.instruction === 'undefined') ||
+    criteria.question.instruction === "") {
+    return ``;
+  } else return `
+    <div class="instructions-row">
+        <span class="label">Instructions</span>
+        <span class="instruction">${criteria.question.instruction}</span>
+      
+    </div>
+    `;
+}
+
+function responsesRow(criteria) {
+
+  var retval;
+
+  if ((typeof criteria.response === "undefined") ||
+    (typeof criteria.response.possibleResponses === "undefined")) {
+    return '';
+  } else {
+    retval = `<div class="responses-row">
+    <div class="response-title">Responses</div>
+    <table class="responses">`;
+
+    retval += criteria.response.possibleResponses.map(
+      (possibleResponse, index) => criteriaResponse(possibleResponse, index)).join('');
+  }
+
+  retval += `</table></div>`;
+
+  return retval;
+}
+
+function criteriaResponse(possibleResponse, index) {
+  console.log("index", index);
+  return `
+  <tr class="possibleResponse">
+    <td class="label ${index ? '' : 'first'}">${possibleResponse.label}:</td> 
+    <td class="meaning  ${index ? '' : 'first'}">${possibleResponse.meaning || ''}</td>
+  </tr>
+  `;
+}
+
+
+function relevanceRow(criteria) {
+  return `
+  <div class="relevance-heading-row">
+    Relevance
+  </div>
+  <div class="relevance-row">
+    ${criteria.relevance}
+  </div>
+  `;
+}
+
+function assessmentsRow(criteria) {
+  if ((typeof criteria.exampleAssessments === "undefined") ||
+    (criteria.exampleAssessments.length === 0)) {
+    return '';
+  } else {
+    return `
+  <div class="assessments-row">
+    Example Assessments
+  </div>
+  ${assessmentsTable(criteria)}
+  `;
+  }
+}
+
+function assessmentsTable(criteria) {
+  return `
+  <table class="assessments">
+    <thead>
+      <tr>
+        ${criteria.assessmentTemplate.columns.map(column => `<th>${column.heading}</th>`).join('')}
+      </tr>
+    </thead>
+    <tbody>
+      ${criteria.exampleAssessments.map(assessmentRow)};
+    </tbody >
+  </table >
+  `
+}
+
+function assessmentRow(assessment, index) {
+  return `
+    <tr>
+      <td>
+      Assessment ${assessment} ${index}
+      </td>
+    </tr>
+    `
+}
+//       let contents = assessment[column.propertyRef] || '';
+//       if (column.propertyRef === 'note' && assessment.evaluationCitation) {
+//         let evalCitation = `[<a href="${assessment.evaluationCitation}">${assessment.evaluationCitation.slice(1)}</a>]`
+
+//         contents += ` ${evalCitation}`;
+//       }
+
+//       return `<td>${contents}</td>`;
+//     }).join('') : ''}
+//                 </tr>
+//                 `).join('')}
+//             </tbody>
+//             </table>
+
+//             </section>` : ''}
+
+//         </section>
+//         `
+
+// }
 
 window.renderCategories = renderCategories;
