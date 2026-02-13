8859ed988573fe9e244da16b656d0200c4e3caa8
Will Abramson <wip.abramson@gmail.com>
2026-02-13 17:08:21 +0000

    define retired criteria array

---
+++ rubric/retired-criteria.js
@@ -0,0 +1,26 @@
+const retiredCriteria = [
+    {
+        "id": "https://www.w3.org/TR/did-rubric#criteria-13",
+        "permalink": "https://www.w3.org/TR/2021/NOTE-did-rubric-20211119/#criteria-13"
+    },
+    {
+        "id": "https://www.w3.org/TR/did-rubric#criteria-14",
+        "permalink": "https://www.w3.org/TR/2021/NOTE-did-rubric-20211119/#criteria-14"
+    },
+    {
+        "id": "https://www.w3.org/TR/did-rubric#criteria-15",
+        "permalink": "https://www.w3.org/TR/2021/NOTE-did-rubric-20211119/#criteria-15"
+    },
+    {
+        "id": "https://www.w3.org/TR/did-rubric#criteria-16",
+        "permalink": "https://www.w3.org/TR/2021/NOTE-did-rubric-20211119/#criteria-16"
+    },
+    {
+        "id": "https://www.w3.org/TR/did-rubric#criteria-17",
+        "permalink": "https://www.w3.org/TR/2021/NOTE-did-rubric-20211119/#criteria-17"
+    },
+    {
+        "id": "https://www.w3.org/TR/did-rubric#criteria-18",
+        "permalink": "https://www.w3.org/TR/2021/NOTE-did-rubric-20211119/#criteria-18"
+    }
+]
