c76dbc90ba32547a78531b3569d83be4c50f5893
Will Abramson <wip.abramson@gmail.com>
2026-02-17 15:02:24 +0000

    move substandard criteria to retired-criteria.js and delete their JSON definition

--- rubric/retired-criteria.js
+++ rubric/retired-criteria.js
@@ -1,5 +1,17 @@
 const retiredCriteria = [
     {
+        "id": "https://www.w3.org/TR/did-rubric#criteria-3",
+        "permalink": "https://www.w3.org/TR/2021/NOTE-did-rubric-20211119/#criteria-3"
+    },
+    {
+        "id": "https://www.w3.org/TR/did-rubric#criteria-4",
+        "permalink": "https://www.w3.org/TR/2021/NOTE-did-rubric-20211119/#criteria-4"
+    },
+    {
+        "id": "https://www.w3.org/TR/did-rubric#criteria-5",
+        "permalink": "https://www.w3.org/TR/2021/NOTE-did-rubric-20211119/#criteria-5"
+    },
+    {
         "id": "https://www.w3.org/TR/did-rubric#criteria-13",
         "permalink": "https://www.w3.org/TR/2021/NOTE-did-rubric-20211119/#criteria-13"
     },
@@ -22,5 +34,37 @@ const retiredCriteria = [
     {
         "id": "https://www.w3.org/TR/did-rubric#criteria-18",
         "permalink": "https://www.w3.org/TR/2021/NOTE-did-rubric-20211119/#criteria-18"
+    },
+        {
+        "id": "https://www.w3.org/TR/did-rubric#criteria-19",
+        "permalink": "https://www.w3.org/TR/2021/NOTE-did-rubric-20211119/#criteria-19"
+    },
+    {
+        "id": "https://www.w3.org/TR/did-rubric#criteria-20",
+        "permalink": "https://www.w3.org/TR/2021/NOTE-did-rubric-20211119/#criteria-20"
+    },
+    {
+        "id": "https://www.w3.org/TR/did-rubric#criteria-21",
+        "permalink": "https://www.w3.org/TR/2021/NOTE-did-rubric-20211119/#criteria-21"
+    },
+    {
+        "id": "https://www.w3.org/TR/did-rubric#criteria-22",
+        "permalink": "https://www.w3.org/TR/2021/NOTE-did-rubric-20211119/#criteria-22"
+    },
+    {
+        "id": "https://www.w3.org/TR/did-rubric#criteria-23",
+        "permalink": "https://www.w3.org/TR/2021/NOTE-did-rubric-20211119/#criteria-23"
+    },
+    {
+        "id": "https://www.w3.org/TR/did-rubric#criteria-26",
+        "permalink": "https://www.w3.org/TR/2021/NOTE-did-rubric-20211119/#criteria-26"
+    },
+    {
+        "id": "https://www.w3.org/TR/did-rubric#criteria-27",
+        "permalink": "https://www.w3.org/TR/2021/NOTE-did-rubric-20211119/#criteria-27"
+    },
+    {
+        "id": "https://www.w3.org/TR/did-rubric#criteria-32",
+        "permalink": "https://www.w3.org/TR/2021/NOTE-did-rubric-20211119/#criteria-32"
     }
 ]
