e48a46fa8abdc3b8a4e84dee167fb2f8299e9204
Will Abramson <wip.abramson@gmail.com>
2026-03-24 17:52:34 +0000

    change evaluations folder path to evaluationCitations

--- tooling/src/utils.js
+++ tooling/src/utils.js
@@ -3,7 +3,7 @@ import path from "path";
 
 export const RUBRIC_ROOT = path.resolve(process.cwd(), "../rubric");
 export const CRITERIA_ROOT = path.resolve(RUBRIC_ROOT, "criteria");
-export const EVALUATIONS_ROOT = path.resolve(RUBRIC_ROOT, "evaluations");
+export const EVALUATIONS_ROOT = path.resolve(RUBRIC_ROOT, "evaluationCitations");
 export const RUBRIC_OUTLINE_FILE = path.join(RUBRIC_ROOT, "rubric-outline.json");
 export const OUTPUT_FILE = path.join(RUBRIC_ROOT, "rubric.js");
 export const EVALUATIONS_OUTPUT_FILE = path.join(RUBRIC_ROOT, "evaluations.js");
